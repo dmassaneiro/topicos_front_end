430709a4ff1f22a119bc3d522446a7205f331e13
Diego Massaneiro <dmassaneiro95@gmail.com>
2017-09-13 21:27:43 -0300

    aula dia 13/09

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import TodoPage from './pages/TodoPage';
+
+class App extends Component {
+
+  render() {
+    return (
+      <main>
+        <TodoPage />
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+/*
+Alisson Igor
+Jose Vinicius
+Bruno Henrique
+Jucelino
+Felipe
+*/
+
+import {
+     Button, Modal, FormGroup, FormControl,
+     ControlLabel,
+} from 'react-bootstrap';
+
+class TodoForm extends Component {
+
+     state = {};
+
+     onTitleChange = (event) => {
+          this.setState({ title: event.target.value });
+     }
+
+     onDescriptionChange = (event) => {
+          this.setState({ description: event.target.value });
+     }
+
+     onSave = () => {
+          const { title , description } = this.state;
+
+          if (!title || !description) {
+               alert("Preencha o titulo e a descrição da tarefa!");
+               return;
+          }
+
+          this.setState({title:"",description:""});
+
+          this.props.onSave(title, description);
+     }
+
+     render() {
+          // const showForm = this.props.showForm;
+          // const onClose = this.props.onClose;
+          // const onSave = this.props.onSave;
+          const { showForm, onClose, onSave } = this.props;
+
+          // const title = this.state.title;
+          // const description = this.state.description;
+          const { title, description } = this.state;
+          return (
+               <Modal show={showForm} onHide={onClose}>
+                    <Modal.Header closeButton>
+                         <Modal.Title>Tarefa</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                         <form>
+                              <FormGroup>
+                                   <ControlLabel>#</ControlLabel>
+                                   <FormControl value="" disabled />
+                              </FormGroup>
+                              <FormGroup>
+                                   <ControlLabel>Título</ControlLabel>
+                                   <FormControl value={title} onChange={this.onTitleChange} />
+                              </FormGroup>
+                              <FormGroup>
+                                   <ControlLabel>Descrição</ControlLabel>
+                                   <FormControl componentClass="textarea" value={description} onChange={this.onDescriptionChange} />
+                              </FormGroup>
+                         </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                         <Button bsSize="small" onClick={onClose}>Cancelar</Button>
+
+                         <Button bsSize="small" bsStyle="info"
+                              onClick={this.onSave}>Salvar</Button>
+                    </Modal.Footer>
+               </Modal>
+          )
+     }
+}
+
+export default TodoForm;
