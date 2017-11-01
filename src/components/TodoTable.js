daf66cb35fc1e5112abda74e12fb9d848e774f5c
Diego Massaneiro <dmassaneiro95@gmail.com>
2017-11-01 21:03:04 -0200

    adicionado elemeto ButtonToolbar Opção Concluida, Pendente

--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 import {
     Table, Button,
-    ButtonGroup,
+    ButtonGroup, ButtonToolbar,ToggleButton,ToggleButtonGroup
 } from 'react-bootstrap';
 
 class TodoTable extends Component {
 
     renderTodo = () => {
-        const { todos, onEditarClick, onExcluirClick } = this.props;
+        const { todos, onEditarClick, onExcluirClick, onConcluidaChange} = this.props;
 
         const todosComponents = todos.map((todo, index) => {
             return (
                 <tr>
                     <td>{todo.id}</td>
                     <td>{todo.title}</td>
-                    <td>{todo.date}</td>
+                    <td>{moment(todo.creation_date).format('DD/MM/YYYY [às] HH:mm')}</td>
                     <td>{todo.completed}</td>
+                    <td>
+                        <ButtonToolbar>
+                            <ToggleButtonGroup bsSize='small' type="radio"
+                                onChange={(concluida) =>onConcluidaChange(todo.id, concluida)}
+                                name="completed" value={todo.completed}>
+                                <ToggleButton value={true}>Concluída</ToggleButton>
+                                <ToggleButton value={false}>Pendente</ToggleButton>
+                            </ToggleButtonGroup>
+                        </ButtonToolbar>
+                    </td>
                     <td>
                         <ButtonGroup bsSize="small">
                             <Button bsStyle="warning"
